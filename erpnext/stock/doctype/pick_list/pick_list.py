b7930644a22b2f95148a4642d2751bfdfa189fc1
lopper <863201+lopper@users.noreply.github.com>
2024-05-09 17:43:06 -0700

    Update pick_list.py
    Select batch based on LIFO

--- erpnext/stock/doctype/pick_list/pick_list.py
+++ erpnext/stock/doctype/pick_list/pick_list.py
@@ -654,7 +654,7 @@ def get_available_item_locations_for_batched_item(
 		)
 		.groupby(sle.warehouse, sle.batch_no, sle.item_code)
 		.having(Sum(sle.actual_qty) > 0)
-		.orderby(IfNull(batch.expiry_date, "2200-01-01"), batch.creation, sle.batch_no, sle.warehouse)
+		.orderby("-" + batch.creation, sle.batch_no, sle.warehouse)
 		.limit(cint(required_qty + total_picked_qty))
 	)
 
