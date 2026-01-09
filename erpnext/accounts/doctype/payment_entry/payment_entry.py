b14b32baa7a1c75a68e78b22983272686b871903
Patrick L <patricklo@gmail.com>
2026-01-09 14:34:33 -0800

    fix: sort payment entries by debit and credit values; update default date range in payment entry form

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -2005,6 +2005,8 @@ def get_outstanding_reference_documents(args):
 
 	data = negative_outstanding_invoices + outstanding_invoices + orders_to_be_billed
 
+	data = sorted(data, key=lambda x: x.posting_date)
+	
 	if not data:
 		if args.get("get_outstanding_invoices") and args.get("get_orders_to_be_billed"):
 			ref_document_type = "invoices or orders"
